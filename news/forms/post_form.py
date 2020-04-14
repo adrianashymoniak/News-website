97ac0da7025e09f8deb7912ee5cc5f46429e9511
Adriana Shymoniak <adrianashymoniak@gmail.com>
2020-04-14 09:51:34 +0300

    Update templates, add new styles

--- news/forms/post_form.py
+++ news/forms/post_form.py
@@ -6,7 +6,12 @@ from news.models import Post
 
 class PostForm(forms.ModelForm):
     title = forms.CharField(max_length=100, required=True,
-                            help_text='Please provide title for your post')
+                            help_text='Please provide title for your post',
+                            widget=forms.TextInput(
+                                attrs={
+                                    'class': 'post-title',
+                                    'placeholder': 'Title',
+                                }))
     description = forms.CharField(widget=FroalaEditor)
 
     class Meta:
